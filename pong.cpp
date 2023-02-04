bed63180e876fae6432745b191ed2950500f95aa
Felipe Pegoraro <fesousapegora03@gmail.com>
2023-02-04 16:37:25 -0300

    refactoring the game

--- pong.cpp
+++ pong.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
-#include <raylib.h>
 #include "./src/utils.h"
 #include "./src/structures.h"
+#include <iostream>
 
 /*
  * Felipe Pegoraro, 1|2/12/2022
@@ -13,43 +12,19 @@ int main(void)
 	InitWindow(WIDTH, HEIGHT, NAME);
 	SetWindowState(FLAG_VSYNC_HINT);
 
-	Score score = { {0, 0}, nullptr };
-	Bullet bullet(300, 300);
-	Player leftPlayer(1, 500);
-	Player rightPlayer(2, 500);
+  Game game;
 
 	while (!WindowShouldClose())
 	{
-		bullet.bulletChangeSpeed();
-
-		if (IsKeyPressed(KEY_R)){
-			bullet = Bullet(300, 300);
-			leftPlayer = Player(1, 500);
-			rightPlayer = Player(2, 500);
-			score = { {0, 0}, nullptr };
-		}
-
+		game.bullet.BulletChangeSpeed();
+    game.ResetGame();
 		BeginDrawing();
 
-			ClearBackground(BLACK);
-			DrawFPS(15, 15);
-			DrawText(TextFormat("%d x %d", score.match[0], score.match[1]), centerX()-40, 15, 18, WHITE);
-			DrawText(TextFormat("speed: %d", abs(bullet.speed.x)), WIDTH-110, 15, 18, WHITE);
-
-			bullet.drawBullet();
-			leftPlayer.drawPlayer();
-			rightPlayer.drawPlayer();
-
-			leftPlayer.handlePlayerMovement();
-			rightPlayer.handlePlayerMovement();
-
-			leftPlayer.handleBulletColision(&bullet);
-			rightPlayer.handleBulletColision(&bullet);
-
-			bullet.handleWinner(&score, &bullet);
-
-			leftPlayer.handleBorderCollision();
-			rightPlayer.handleBorderCollision();
+      game.DrawGame();
+      game.HandleMoviment();
+      game.HandleColision();
+			game.bullet.HandleWinner(&game.score, &game.bullet);
+      game.HandleBorderColision();
 
 		EndDrawing();
 	}
--- src/structures.cpp
+++ src/structures.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <raylib.h>
 
-void Bullet::bulletChangeSpeed(void)
+void Bullet::BulletChangeSpeed(void)
 {
 	position.x += speed.x * GetFrameTime();
 	position.y += speed.y * GetFrameTime();
@@ -12,18 +12,18 @@ void Bullet::bulletChangeSpeed(void)
 
 	if (position.y < 0)
 	{
-		position.y = 0;
-		speed.y *= -1;
+		position.y  =  0;
+		speed.y    *= -1;
 	}
 
 	if (position.y > currentHeight)
 	{
-		position.y = currentHeight;
-		speed.y *= -1;
+		position.y  = currentHeight;
+		speed.y    *= -1;
 	}
 }
 
-void Bullet::drawBullet(void)
+void Bullet::DrawBullet(void)
 {
 	DrawCircle(
 		position.x,
@@ -33,95 +33,101 @@ void Bullet::drawBullet(void)
 	);
 }
 
-void Bullet::handleWinner(Score *sc, Bullet *bl)
+void Bullet::HandleWinner(Score *sc, Bullet *bl)
 {
+  const int minWP = 3;
+
 	if (position.x > WIDTH)
 	{
 		*bl = Bullet(300, 300);
-		if (++(sc->match[0]) == 3){
-			sc->winner = "Left Player";
-		}
+		if (++(sc->match[0]) >= minWP && sc->match[0]-sc->match[1]>=2){
+      sc->winner = "Left Player";
+    }
 	}
 
 	if (position.x < 0)
 	{
 		*bl = Bullet(300, 300);
-		if(++(sc->match[1]) == 3){
-			sc->winner = "Right Player";
-		}
+		if(++(sc->match[1]) >= minWP && sc->match[1]-sc->match[0]>=2){
+      sc->winner = "Right Player";
+    }
 	}
 
 	if (sc->winner) 
 	{
 		DrawText(TextFormat("%s Wins", sc->winner), 180, centerY()-15, 30, BLUE);
-		bl->speed.x = 0;
-		bl->speed.y = 0;
+    bl->speed  = {0, 0};
 		bl->radius = 0;
 		return;
 	}
 }
 
-void Player::handlePlayerMovement(void)
+void Player::HandlePlayerMovement(void)
 {
-	int keys[2] = { KEY_UP, KEY_DOWN };
+  Point<int> keys = { KEY_UP, KEY_DOWN };
 
-	if (side == 1)
+  if (side == 1)
 	{
-		keys[0] = KEY_W;
-		keys[1] = KEY_S;
+		keys.x = KEY_W;
+		keys.y = KEY_S;
 	}
 
-	if (IsKeyDown(keys[0]))
-		position.y -= speed * GetFrameTime();
+  if (IsKeyDown(keys.x))
+    position.y -= speed * GetFrameTime();
 
-	if (IsKeyDown(keys[1]))
-		position.y += speed * GetFrameTime();
+  if (IsKeyDown(keys.y))
+    position.y += speed * GetFrameTime();
 }
 
-Rectangle Player::getRectangleFromPlayer()
+Rectangle Player::GetRectangleFromPlayer()
 {
 	return Rectangle { 
-		(float) position.x - width/2,
+		(float) position.x -  width/2,
 		(float) position.y - height/2,
 		(float) width,
 		(float) height 
 	};
 }
 
-void Player::drawPlayer(void)
+void Player::DrawPlayer(void)
 {
 	DrawRectangleRec(
-		getRectangleFromPlayer(),
+		GetRectangleFromPlayer(),
 		WHITE
 	);
 }
 
-void Player::handleBulletColision(Bullet *bullet)
+void Player::HandleBulletColision(Bullet *bullet)
 {
+  const double upSpeed  =  1.16;
+  const double maxSpeed = 550.0;
+
 	if (CheckCollisionCircleRec(
 			Vector2{ (float)bullet->position.x, (float)bullet->position.y },
 			bullet->radius,
-			getRectangleFromPlayer()
+			GetRectangleFromPlayer()
 		))
 	{
 		if (side == 1){
 			if (bullet->speed.x < 0){
-				bullet->speed.x *= -1.06;
-				bullet->speed.y = (bullet->position.y - position.y)/(height/2) * bullet->speed.x;
+				bullet->speed.x *= (bullet->speed.x > maxSpeed) ? -upSpeed : -1;
+				bullet->speed.y  = (bullet->position.y - position.y)/(height/2) * bullet->speed.x;
 			}
 		}
 
 		if (side == 2){
 			if (bullet->speed.x > 0){
-				bullet->speed.x *= -1.06;
-				bullet->speed.y = (bullet->position.y - position.y)/(height/2) * -bullet->speed.x;
+				bullet->speed.x *= (bullet->speed.x > maxSpeed) ? -1 : -upSpeed;
+				bullet->speed.y  = (bullet->position.y - position.y)/(height/2) * -bullet->speed.x;
 			}
 		}
 	}
 }
 
-void Player::handleBorderCollision(void)
+void Player::HandleBorderCollision(void)
 {
+  const int scHeight = GetScreenHeight();
+
 	double pos[2] = {
 		position.y - height/2,
 		position.y + height/2
@@ -130,6 +136,54 @@ void Player::handleBorderCollision(void)
 	if (pos[0] <= 0.0f) 
 		position.y = height/2;
 
-	if (pos[1] >= GetScreenHeight())
-		position.y = GetScreenHeight()-height/2;
+	if (pos[1] >= scHeight)
+		position.y = scHeight-height/2;
+}
+
+void Game::DrawGame(void){
+  ClearBackground(BLACK);
+  DrawFPS(15, 15);
+
+  DrawText(
+    TextFormat("%d x %d", score.match[0], score.match[1]),
+    centerX()-40,
+    15, 18,
+    WHITE
+  );
+
+  DrawText(
+    TextFormat("speed: %d", abs(bullet.speed.x)),
+    WIDTH-110,
+    15, 18,
+    WHITE
+  );
+
+  bullet.DrawBullet();
+  leftPlayer.DrawPlayer();
+  rightPlayer.DrawPlayer();
+}
+
+void Game::ResetGame(void){
+  if (IsKeyPressed(KEY_R)){
+    bullet = Bullet(300, 300);
+    leftPlayer = Player(1, 500);
+    rightPlayer = Player(2, 500);
+    score = { {0, 0}, nullptr };
+  }
+}
+
+void Game::HandleMoviment(void)
+{
+  leftPlayer.HandlePlayerMovement();
+  rightPlayer.HandlePlayerMovement();
+}
+
+void Game::HandleColision(){
+  leftPlayer.HandleBulletColision(&bullet);
+  rightPlayer.HandleBulletColision(&bullet);
+}
+
+void Game::HandleBorderColision(void){
+  leftPlayer.HandleBorderCollision();
+  rightPlayer.HandleBorderCollision();
 }
